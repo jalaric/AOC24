048501d31cd78a8d0447e43acf518b59b8c8d188
jalaric <julien.alaric@gmail.com>
2024-12-07 09:40:06 +0100

    Add day5 part 2

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -35,19 +35,19 @@ fn get_pair(lines: &mut Vec<String>, pair: &mut Vec<Pair_t>, i: usize) -> usize
     return i;
 }
 
-fn is_number_after(number_vector : &Vec<u32>, current_index: usize, previous_num: u32) -> bool
+fn is_number_after(number_vector : &Vec<u32>, current_index: usize, previous_num: u32) -> i32
 {
     for i in current_index..number_vector.len()
     {
         if(number_vector[i] == previous_num)
         {
-            return false;
+            return i as i32;
         }
     }
-    true
+    -1
 }
 
-fn check_update(lines: &mut Vec<String>, pair: &Vec<Pair_t>, i:usize) -> u32
+fn check_update(lines: &Vec<String>, pair: &Vec<Pair_t>, i:usize) -> u32
 {
     if(i >= lines.len()){
         return 0;
@@ -71,14 +71,68 @@ fn check_update(lines: &mut Vec<String>, pair: &Vec<Pair_t>, i:usize) -> u32
         for z in 0..number_vec.len() {
             for j in 0..pair.len(){
                 if pair[j].1 == number_vec[z] {
-                    if(!is_number_after(&number_vec, z, pair[j].0)){
+                    if(-1 != is_number_after(&number_vec, z, pair[j].0)){
                         return check_update(lines, pair, i + 1);
                     }
                 }
             }
         }
-        //println!("Valid update : {}", lines[i]);
         return number_vec[number_vec.len() / 2] + check_update(lines, pair, i + 1);
+    }
+}
+
+fn check_update_with_correction(lines:& Vec<String>, pair: &Vec<Pair_t>, i:usize) -> u32
+{
+    let mut was_wrong = false;
+    if(i >= lines.len()){
+        return 0;
+    }else{
+        let char_vec: Vec<char> = lines[i].chars().collect();
+        let mut number_vec= Vec::new();
+        let mut count = 0;
+        let mut num: u32 = 0;
+        while(char_vec.len() > count && char_vec[count].is_digit(10)){
+            num *= 10;
+            num += char_vec[count].to_digit(10).unwrap();
+            count+=1;
+            if char_vec.len() > count && char_vec[count] == ','{
+                number_vec.push(num);
+                count+=1;
+                num = 0;
+            } else if !(char_vec.len() > count) {
+                number_vec.push(num);
+            }
+        }
+        let mut z = 0;
+        let mut swap = false;
+        while (z < number_vec.len()){
+            for j in 0..pair.len(){
+                if pair[j].1 == number_vec[z] {
+                    let index_to_swap = is_number_after(&number_vec, z, pair[j].0);
+                    if(-1 != index_to_swap){
+                        //swap the numbers
+                        number_vec[z] += number_vec[index_to_swap as usize];
+                        number_vec[index_to_swap as usize] = number_vec[z] - number_vec[index_to_swap as usize];
+                        number_vec[z] -= number_vec[index_to_swap as usize];
+                        swap = true;
+                        break;
+                    }
+                }
+            }
+            if(!swap){
+                z += 1;
+            }else{
+                was_wrong = true;
+                z = 0;
+                swap = false;
+            }
+        }
+        //println!("Valid update : {:?}", number_vec);
+        if(was_wrong){
+            return number_vec[number_vec.len() / 2] + check_update_with_correction(lines, pair, i + 1);
+        }else{
+            return check_update_with_correction(lines, pair, i + 1);
+        }
     }
 }
 
@@ -86,6 +140,8 @@ fn main() {
     let mut lines = read_lines("day5.data");
     let mut pair_vector = Vec::new();
     let start = get_pair(&mut lines, &mut pair_vector, 0) + 1;
-    let result = check_update(&mut lines, &pair_vector, start);
+    let result = check_update(&lines, &pair_vector, start);
     println!("Result part 1: {}", result);
+    let result_2 = check_update_with_correction(&lines, &pair_vector, start);
+    println!("Result part 2: {}", result_2);
 }
